8ded2ee396a7f9c18d2e7701168938339769c525
Michael Weibel <michael.weibel@gmail.com>
2012-02-29 12:49:44 +0100

    strophe.js plugin for webrtc using jingle

--- jingle/strophe.jingle.js
+++ jingle/strophe.jingle.js
@@ -12,33 +12,26 @@
 
 if (window.webkitPeerConnection) {
 	window.PeerConnection = window.webkitPeerConnection;
+	window.URL = window.webkitURL;
+	navigator.getUserMedia = navigator.webkitGetUserMedia;
 }
 
 Strophe.addConnectionPlugin('jingle', {
-	/** Constant: PAYLOAD_TYPES
-	 * Payload types according to WebRTC documentation
-	 * 
-	 * The ID Attribute (payload type) is not known to me, so correct it if you know
-	 * the correct id.
-	 */
-	PAYLOAD_TYPES: {
-		video: [
-			{name: 'VP8', 'clockrate': '90000', id: '98'}
-		],
-		audio: [
-			{name: 'isac', 'clockrate': '16000', id: '105'},
-			{name: 'isac', 'clockrate': '32000', id: '106'},
-			{name: 'iLBC', 'clockrate': '8000', id: '102'}
-		]
-	},
 	STATUS: {
 		BUSY: 1
 	},
 
 	_connection: null,
 	_peerConnection: null,
+	_sdpData: {},
+	_sdpMessage: "",
 	_status: null,
 	_serverConfig: "NONE",
+	_localView: null,
+	_localStream: null,
+	_remoteView: null,
+	_remoteStream: null,
+	_receiver: false, // indicates if client is receiver or sender
 
 	init: function(conn) {
 		this._connection = conn;
@@ -51,9 +44,12 @@ Strophe.addConnectionPlugin('jingle', {
 		Strophe.addNamespace('JINGLE_RTP_AUDIO', 'urn:xmpp:jingle:apps:rtp:audio');
 		Strophe.addNamespace('JINGLE_RTP_VIDEO', 'urn:xmpp:jingle:apps:rtp:video');
 		Strophe.addNamespace('JINGLE_TRANSPORTS_ICE_UDP', 'urn:xmpp:jingle:transports:ice-udp:1');
+		Strophe.addNamespace('JINGLE_TRANSPORTS_RAW_UDP', 'urn:xmpp:jingle:transports:raw-udp:1');
+		Strophe.addNamespace('JINGLE_TRANSPORTS_WEBRTC', 'urn:xmpp:jingle:transports:webrtc:1');
 
 		this._connection.disco.addFeature(Strophe.NS.JINGLE);
-		this._connection.disco.addFeature(Strophe.NS.JINGLE);
+		this._connection.disco.addFeature(Strophe.NS.JINGLE_TRANSPORTS_WEBRTC);
+		this._connection.disco.addFeature(Strophe.NS.JINGLE_RTP);
 		this._connection.disco.addFeature(Strophe.NS.JINGLE_RTP_AUDIO);
 		this._connection.disco.addFeature(Strophe.NS.JINGLE_RTP_VIDEO);
 	},
@@ -70,6 +66,51 @@ Strophe.addConnectionPlugin('jingle', {
 		this._serverConfig = config;
 	},
 
+	setLocalView: function(el) {
+		this._localView = el;
+		this._getUserMedia();
+	},
+
+	setRemoteView: function(el) {
+		this._remoteView = el;
+	},
+
+	_createPeerConnection: function(sdpMessageCallback) {
+		var self = this;
+		this._peerConnection = new PeerConnection(this._serverConfig, sdpMessageCallback);
+		this._peerConnection.onaddstream = function(e) {
+			console.log("ADDSTREAM");
+			console.log(e);
+			var stream = e.stream,
+				url = URL.createObjectURL(stream);
+			self._remoteView.attr('src', url);
+			self._remoteStream = stream;
+		}
+		this._peerConnection.onremovestream = function(e) {
+			console.log("REMOVESTREAM");
+			console.log(e);
+			self._remoteView.attr('src', '');
+		}
+		this._peerConnection.addStream(this._localStream);
+	},
+
+	_getUserMedia: function() {
+		navigator.getUserMedia("video,audio", this._gotStream.bind(this), this._gotStreamFailed.bind(this));
+	},
+
+	_gotStream: function(s) {
+		console.log("GOTSTREAM");
+		console.log(s);
+		var url = URL.createObjectURL(s);
+		this._localView.attr('src', url);
+		this._localStream = s;
+	},
+
+	_gotStreamFailed: function(error) {
+		console.log("STREAMFAILED");
+		console.log(error);
+	},
+
 	/** Function: initSession
 	 * Sends a session-initialize request to the specified recipient.
 	 *
@@ -82,15 +123,57 @@ Strophe.addConnectionPlugin('jingle', {
 	initSession: function(to, name, media, cb) {
 		var self = this;
 		this._status = this.STATUS.BUSY;
-		this._peerConnection = new PeerConnection(this._serverConfig, function(msg) {
+		this._to = to;
+		this._sid = Math.random().toString(36).substr(10,20);
+		this._createPeerConnection(function(msg) {
+			if (msg.indexOf('OK') !== -1) {
+				var iq = $iq({'from': self._connection.jid, 'to': self._to, 'type': 'set'});
+				iq.c('jingle', {
+					xmlns: Strophe.NS.JINGLE,
+					action: 'session-info',
+					initiator: self._connection.jid,
+					sid: self._sid
+				});
+				iq.c('webrtc', {xmlns: Strophe.NS.JINGLE_RTP});
+				iq.t(msg);
+				self._connection.sendIQ(iq);
+			}
+			if (self._sdpMessage !== "") {
+				return;
+			}
+			self._sdpMessage = msg;
+
 			var iq = $iq({'from': self._connection.jid, 'to': to, 'type': 'set'});
-			iq.c('jingle', {'xmlns': Strophe.NS.JINGLE, 'action': 'session-initiate', 'initiator': self._connection.jid, 'responder' : to, 'sid': 'HAS_TO_BE_GENERATED'});
-			iq.c('content', {'creator': 'initiator', 'name': name, 'senders': 'both'});
-			iq.c('description', {'xmlns': Strophe.NS.JINGLE_RTP, 'media': media});
-			self._attachPayloadTypesAccordingToMedia(iq, media);
-			self._attachTransport(iq.up(), msg);
-			self._connection.sendIQ(iq, cb);
+			iq.c('jingle', {
+				'xmlns': Strophe.NS.JINGLE, 
+				'action': 'session-initiate', 
+				'initiator': self._connection.jid, 
+				'responder' : to, 
+				'sid': self._sid
+			});
+			iq.c('content', {'creator': self._connection.jid, 'name': name, 'senders': 'both'});
+
+			iq.c('transport', {xmlns: Strophe.NS.JINGLE_TRANSPORTS_WEBRTC});
+
+			var id = self._connection.sendIQ(iq, cb);
+			self._connection.addHandler(self._handleSessionInitAccept.bind(self), null, 'iq', 'result', id);
 		});
+	},
+
+	_handleSessionInitAccept: function(stanza) {
+		var iq = $iq({'from': this._connection.jid, 'to': stanza.getAttribute('from'), 'type': 'set'});
+		iq.c('jingle', {
+			xmlns: Strophe.NS.JINGLE,
+			action: 'session-info',
+			initiator: this._connection.jid,
+			sid: this._sdpData.offererSessionId
+		});
+		iq.c('webrtc', {xmlns: Strophe.NS.JINGLE_RTP});
+		iq.t(this._sdpMessage);
+
+		this._connection.sendIQ(iq);
+
+		return false;
 	},
 
 /* --Has to be done by the implementor--
@@ -125,81 +208,50 @@ Strophe.addConnectionPlugin('jingle', {
 			'id': stanza.getAttribute('id'),
 			'type': 'result'
 		}));
+		this._connection.addHandler(this._handleSessionInfo, Strophe.NS.JINGLE, 'iq', 'set');
 
-		var jingle = stanza.querySelector('jingle'),
-			initiator = jingle.getAttribute('initiator'),
-			sid = jingle.getAttribute('sid'),
-			iq = $iq({
-				'from': this._connection.jid,
-				'to': stanza.getAttribute('from'),
-				'type': 'set'
-			});
-		iq.c('jingle', {
-			'xmlns': Strophe.NS.JINGLE,
-			'initiator': initiator,
-			'sid': sid
-		});
-		if (!this._status !== this.STATUS.BUSY) {
-			this._status = this.STATUS.BUSY;
-			iq.attrs({
-				'action': 'session-accept',
-				'responder': this._connection.jid
-			});
-			var content = jingle.querySelector('content');
-			iq.c('content', {
-				'creator': 'initiator',
-				'name': content.getAttribute('name')
-			});	
-			var description = content.querySelector('description'),
-				media = description.getAttribute('media'),
-				payloadTypes = description.childNodes,
-				payloadTypesLength = payloadTypes.length;
-			iq.c('description', {
-				'xmlns': Strophe.NS.JINGLE_RTP,
-				'media': media
-			});
-			var payloadTypeSupported = false;
-			for (var i = 0; i < payloadTypesLength; i++) {
-				var curPayloadType = {},
-					attrs = payloadTypes[i].attributes,
-					attrsLen = attrs.length;
-				for (var a = 0; a < attrsLen; a++) {
-					curPayloadType[attrs[a].name] = attrs[a].value;
+		return true;
+	},
+
+	handleSessionInfo: function(stanza) {
+		var self = this,
+			$stanza = $(stanza),
+			$jingle = $stanza.children('jingle'),
+			iq = $iq({'from': this._connection.jid, 'to': stanza.getAttribute('from'), 'type': 'set'});
+		if (!this._peerConnection) {
+			console.log("SESSION INFO");
+			this._receiver = true;
+			this._createPeerConnection(function(msg) {
+				console.log(msg);
+				if (msg.indexOf('answererSessionId') !== -1) {
+					self._sdpMessage = msg;
+					iq.c('jingle', {
+						xmlns: Strophe.NS.JINGLE,
+						action: 'session-info',
+						initiator: $jingle.attr('initiator'),
+						sid: $jingle.attr('sid')
+					});
+					iq.c('webrtc', {xmlns: Strophe.NS.JINGLE_RTP});
+					iq.t(self._sdpMessage);
+					console.log("MYMSG: ", msg);
+					self._connection.sendIQ(iq);
 				}
-				if (self._payloadTypeSupported(media, curPayloadType)) {
-					iq.c('payload-type', curPayloadType).up();
-					payloadTypeSupported = true;
-				}
-			}
+			});
 
-			if (payloadTypeSupported) {
-				this._peerConnection = new PeerConnection(this._serverConfig, function(msg) {
-					self._attachTransport(iq.up(), msg);
-					var id = self._connection.sendIQ(iq);
-					self._connection.addHandler(cb, Strophe.NS.JINGLE, 'iq', 'set', id, stanza.getAttribute('from'));
-				});
-			} else {
-				var iq = $iq({
-					'from': this._connection.jid,
-					'to': stanza.getAttribute('from'),
-					'type': 'set',
-				});
+			this._peerConnection.processSignalingMessage($stanza.find('webrtc').text());
 
-				iq.c('jingle', {
-					'xmlns': Strophe.NS.JINGLE,
-					'inititator': inititator,
-					'sid': sid,
-					'action': 'session-terminate'
-				});
-				iq.c('reason').c('failed-application');
-				this._connection.sendIQ(iq, cb);
-				this._status = null;
-			}
 		} else {
-			iq.attrs({'action': 'session-terminate'});
-			iq.c('reason').c('busy');
-			this._connection.sendIQ(iq, cb);
-			this._status = null;
+			console.log("PARSING FOOBAR");
+			this._peerConnection.processSignalingMessage($stanza.find('webrtc').text());
+			iq.c('jingle', {
+				xmlns: Strophe.NS.JINGLE,
+				action: 'session-accept',
+				initiator: $jingle.attr('initiator'),
+				sid: $jingle.attr('sid')
+			});
+			iq.c('content', {'creator': $jingle.attr('initiator')});
+			iq.c('transport', {xmlns: Strophe.NS.JINGLE_TRANSPORTS_WEBRTC});
+			this._connection.sendIQ(iq);
 		}
 
 		return true;
@@ -224,144 +276,5 @@ Strophe.addConnectionPlugin('jingle', {
 
 		return true;
 	},
-
-	/** Function: handleSessionAcceptResponse
-	 * After initiator sent the session acceptance acknowledgement, responder
-	 * tries to establish connectivity using the data channel.
-	 * 
-	 * Parameters:
-	 *   (XMLNode) stanza - Session accept acknowledgement message
-	 *   (Function) cb - Callback after connectivity has been established
-	 */
-	handleSessionAcceptResponse: function(stanza, cb) {
-		//TODO
-		cb();
-	},
-
-	/** PrivateFunction: _attachTransport
-	 * Attach transport information to iq
-	 *
-	 * Parameters:
-	 *   (Strophe.Builder) content - Child-Node "content" of iq
-	 *   (String) msg - SDP Message
-	 */
-	_attachTransport: function(content, msg) {
-		msg = this._parsePeerConnectionMessage(msg);
-		content.c('transport', {'xmlns': Strophe.NS.JINGLE_TRANSPORTS_ICE_UDP, 'pwd': msg.pwd, 'ufrag': msg.ufrag});
-			
-		var candidatesLength = msg.candidates.length;
-		for(var i = 0; i < candidatesLength; i++) {
-			content.c('candidate', msg.candidates[i]).up();
-		}
-	},
-
-	/** PrivateFunction: _payloadTypeSupported
-	 * Checks if specified payload type is supported
-	 *
-	 * FIXME: This is ugly & maybe not the correct way on how to do it.
-	 *
-	 * Parameters:
-	 *   (String) media - audio/video
-	 *   (Object) payloadType - Payload type to check
-	 *
-	 * Returns:
-	 *   (Boolean) - true/false
-	 */
-	_payloadTypeSupported: function(media, payloadType) {
-		var supportedPayloadTypesLength = this.PAYLOAD_TYPES[media].length;
-		var supported = false;
-		for (var i = 0; i < supportedPayloadTypesLength; i++) {
-			var curPayloadType = this.PAYLOAD_TYPES[media][i];
-			for(var key in curPayloadType) {
-				if(payloadType[key] === curPayloadType[key]) {
-					supported = true;
-				} else {
-					supported = false;
-				}
-			}
-			if (supported) {
-				return true;
-			}
-		}
-		return false;
-	},
-
-	/** PrivateFunction: _parsePeerConnectionMessage
-	 * Parse peer connection msg from STUN/TURN Server
-	 *
-	 * This parsing was done using only one example (from the
-	 * built-in STUN server of ejabberd) so it may be incomplete
-	 * inacurrate and also the code is ugly.
-	 *
-	 * Improving it would be nice :)
-	 *
-	 * Parameters:
-	 *   (String) msg - Reply from the STUN/TURN Server
-	 *
-	 * Returns:
-	 *   (Object) - Object containing parsed connection message
-	 */
-	_parsePeerConnectionMessage: function(msg) {
-		var lines = msg.split("\n"),
-			protocol = lines[0],
-			lineCount = lines.length,
-			ret = {
-				'protocol': protocol,
-				'candidates': []
-			};
-		for(var i = 1; i < lineCount; i++) {
-			var line = lines[i].split("=");
-			if (!line[1]) {
-				continue;
-			}
-			line = line[1].split(" ");
-			var identifier = line[0].split(":");
-			if (identifier[0] == "candidate") {
-				var id = MD5.hexdigest(line[2] + line[3] + line[4] + line[5] + line[7]);
-				ret.candidates.push({
-					'foundation': identifier[1],
-					'generation': 0, //FIXME
-					'network': 0, // FIXME
-					'component': line[1],
-					'protocol': line[2],
-					'priority': line[3],
-					'ip': line[4],
-					'port': line[5],
-					'type': line[7],
-					'id': id
-				});
-			} else if(identifier[0] == "ice-ufrag") {
-				ret.ufrag = identifier[1];
-			} else if(identifier[0] == "ice-pwd") {
-				ret.pwd = identifier[1];
-			}
-		}
-
-		return ret;
-	},
-
-	/** PrivateFunction: _attachPayloadTypesAccordingToMedia
-	 * Attach payload types to the description-node
-	 *
-	 * Maybe put those payload-types into a user-overridable constant.
-	 * Parameters:
-	 *   (Strophe.Builder) description - Description node
-	 *   (String) media - Media type (audio/video)
-	 */
-	_attachPayloadTypesAccordingToMedia: function(description, media) {
-		var payloadTypes = [];
-		switch(media) {
-			case 'video':
-				payloadTypes = this.PAYLOAD_TYPES.video;
-				break;
-			case 'audio':
-				payloadTypes = this.PAYLOAD_TYPES.audio;
-				break;
-		}
-		var payloadTypesLength = payloadTypes.length;
-		for(var i = 0; i < payloadTypesLength; i++) {
-			description.c('payload-type', payloadTypes[i]).up();
-		}
-	}
 
  });
